22893d4e5cc480550dbb726ee3bdeb474d6f2652
Matenia Rossides <matenia@gmail.com>
2012-06-09 16:31:25 +1000

    seeds and migrations

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+ary = %w(Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.).map { |s| s.downcase.gsub('.', '') }
+
+len = ary.count
+
+ary.each do |word|
+  Note.find_or_create_by_name(:name => word, :tag => ary[rand(len -1)])
+end
